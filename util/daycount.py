0b4d547ca31b7926e59365769edf07dfae57456a
l-plantarum <katsumi@katsumi.wpac4x1hui3uthvkjjsep3ph0h.bx.internal.cloudapp.net>
2018-01-17 12:31:36 +0000

    日付をYYYY/MM/DDに統一したのでそちらに合わせた

--- util/daycount.py
+++ util/daycount.py
@@ -11,12 +11,12 @@ db = client.local
 # 2017/12/30からデータ収集を開始した
 beginDate = datetime.date(2017, 12, 30)
 today = datetime.date.today()
-workday = today
 oneday = datetime.timedelta(days = 1)
+workday = today + datetime.timedelta(hours=9) + oneday
 
 while (workday >= beginDate):
 	# YYYY/MM/DD
-	theday = "{0:%Y/%m/%d}".format(workday).replace("/0","/")
+	theday = "{0:%Y/%m/%d}".format(workday)
 	# 投稿月・投稿日の形式注意(○2018/1/2 ×2018/01/02)
 	beginTime = theday + " 00:00:00"
 	endTime   = theday + " 23:59:59"

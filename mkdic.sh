b7f0fb699d18e9b1c8c53ef640a25c44c823af87
l-plantarum <katsumi@DESKTOP-5UJD8Q1.localdomain>
2019-05-03 12:35:20 +0900

    パスをWSLのパスに変更

--- mkdic.sh
+++ mkdic.sh
@@ -4,4 +4,4 @@ python3 mkdic.py dic/department.txt
 python3 mkdic.py dic/subject.txt
 python3 mkdic.py dic/slang.txt
 cat dic/univ.csv dic/undergraduate.csv dic/department.csv dic/subject.csv slang.csv > dic/univterm.csv 
-/usr/local/libexec/mecab/mecab-dict-index -d/usr/local/lib/mecab/dic/ipadic -u dic/univterm.dic -f utf8 -t utf8 dic/univterm.csv
+/usr/share/libexec/mecab/mecab-dict-index -d/usr/share/lib/mecab/dic/ipadic -u dic/univterm.dic -f utf8 -t utf8 dic/univterm.csv

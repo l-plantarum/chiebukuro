bf4f83489d133070c76e162e87bc3b85591f6541
l-plantarum <katsumi@DESKTOP-5UJD8Q1.localdomain>
2019-05-05 23:17:49 +0900

    ○つき数字の追加

--- mkdic.sh
+++ mkdic.sh
@@ -1,7 +1,7 @@
-python3 mkdic.py dic/univ.txt univ
-python3 mkdic.py dic/undergraduate.txt
-python3 mkdic.py dic/department.txt
-python3 mkdic.py dic/subject.txt
-python3 mkdic.py dic/term.txt
-cat dic/univ.csv dic/undergraduate.csv dic/department.csv dic/subject.csv term.csv > dic/univterm.csv 
-/usr/local/libexec/mecab/mecab-dict-index -d/usr/local/lib/mecab/dic/ipadic -u dic/univterm.dic -f utf8 -t utf8 dic/univterm.csv
+#python3 mkdic.py dic/univ.txt univ
+#python3 mkdic.py dic/undergraduate.txt
+#python3 mkdic.py dic/department.txt
+#python3 mkdic.py dic/subject.txt
+#python3 mkdic.py dic/term.txt
+cat dic/univ.csv dic/undergraduate.csv dic/department.csv dic/subject.csv dic/term.csv maru.csv > dic/univterm.csv 
+/usr/lib/mecab/mecab-dict-index -d/usr/local/src/mecab-ipadic-neologd/build/mecab-ipadic-2.7.0-20070801-neologd-20190425/ -u dic/univterm.dic -f utf8 -t utf8 dic/univterm.csv

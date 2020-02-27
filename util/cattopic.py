7bb6ad3cf258ed783668532495102e3674232453
l-plantarum <katsumi@DESKTOP-5UJD8Q1.localdomain>
2020-02-27 23:00:18 +0900

    オプションを付けて汎用的に指定したトピックが表示できるようにした

--- util/cattopic.py
+++ util/cattopic.py
@@ -3,26 +3,47 @@
 
 from pymongo import MongoClient
 import sys
+import getopt
 
 testdb = "test20200113"
 qadb = "qa20200113"
+begindate = "2018/01/01"
+enddate = "2019/12/31"
+ntopic = "150"
+topicid = -1
+prob = "0.6"
+
+opts, args = getopt.getopt(sys.argv[1:], "n:f:t:i:p:h",  longopts=[])
+
+for opt, arg in opts:
+	if opt == "-h":
+		print("[usage] cattopic.py [-n {150|20}] [-f <from>] [-t <to>] -i <topicid> -p <probability>\n")
+		sys.exit(0)
+	if opt == "-n":
+		if arg != "150" and arg != "20":
+			print("-n 150 or -n 20")
+			sys.exit(1)
+		ntopic = arg
+	if opt == "-f":
+		begindate = arg
+	if opt == "-t":
+		enddate = arg
+	if opt == "-i":
+		topicid = int(arg)
+	if opt == "-p":
+		prob = arg
+
+if topicid == -1:
+	print("topicid must be specified\n")
+	sys.exit(1)
 
 client = MongoClient('mongodb://localhost:27017')
 db = client.local
 
-# cattopic.py {150|20} id th
-
-topic = int(sys.argv[2])
-prob = sys.argv[3]
-posts = db[testdb].find({"maxtopic150":"34","maxprob150":{"$gte":"0.5"}}).sort([{"postdate", "1"}])
-
-if sys.argv[1] == "150":
-	posts = db[testdb].find({"maxtopic150": topic, "maxprob150": {"$gte": prob}}, {"_id"})
-elif sys.argv[1] == "20":
-	posts = db[testdb].find({"maxtopic20": topic, "maxprob20": {"$gte": prob}}, {"_id"})
-else:
-	print("cattopic.py {150|20} <topicid> <probablity>")
-	sys.exit(0)
+if ntopic == "150":
+	posts = db[testdb].find({"maxtopic150": topicid, "maxprob150": {"$gte": prob}, "postdate": {"$gte": begindate, "$lte": enddate}}, {"_id"})
+if ntopic == "20":
+	posts = db[testdb].find({"maxtopic20": topicid, "maxprob20": {"$gte": prob}, "postdate": {"$gte": begindate, "$lte": enddate}}, {"_id"})
 
 for p in posts:
 	id = p	
